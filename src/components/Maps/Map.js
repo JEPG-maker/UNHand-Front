a765c2d9a3d08fef480cb30a720186a84bd18826
Colmirage <semolinag@unal.edu.co>
2023-10-26 16:23:08 -0500

    version 4 fix 1 del mapa

--- src/components/Maps/Map.js
+++ src/components/Maps/Map.js
@@ -1,7 +1,6 @@
-import { useEffect, useMemo } from "react";
+import { useState, useMemo } from "react";
 import { GoogleMap, useLoadScript, MarkerF, InfoWindow } from "@react-google-maps/api";
 import './Map.css';
-
 
 
 export default function Ofertass() {
@@ -15,28 +14,48 @@ export default function Ofertass() {
 
 function Map() {
   const datos = [
-    {lat: 4.666,lng: -74.084, name: '1'},
-    {lat: 4.707,lng: -74.107, name: '2'},
-    {lat: 4.666,lng: -74.107, name: '3'},
-    {lat: 4.645,lng: -74.107, name: '4'}
+    {posicion:{lat: 4.666,lng: -74.084}, name: '1', description: 'a', requirement:'qwe', cash: '123'},
+    {posicion:{lat: 4.675,lng: -74.100}, name: '2', description: 'b', requirement:'rty', cash: '312'},
+    {posicion:{lat: 4.666,lng: -74.107}, name: '3', description: 'c', requirement:'sdf', cash: '345'},
+    {posicion:{lat: 4.645,lng: -74.112}, name: '4', description: 'd', requirement:'xcv', cash: '654'}
   ];
 
+  const [info, setInfo] = useState(false)
+
   const center = useMemo(() => ({lat: 4.659372152302655,lng: -74.09146471557196}), []);
+
+  const handleMouseOver = (e, posicion) => {
+    //if (info === false) {
+      setInfo(posicion)
+      console.log(info)
+    } //else setInfo(false)
+      //console.log(info)
+    //setInfo(posicion)
+  
+
   
 
   return (
     <>
-      <h2>Bienvenido</h2>
-      <GoogleMap zoom={12} center={center} mapContainerClassName="map-container">
+      <h2 className="bienvenida">Bienvenido</h2>
+      <GoogleMap zoom={15} center={center} mapContainerClassName="map-container">
         <div>
           {
             datos.map((e) => 
-            <MarkerF position={e}>
-              <InfoWindow>
-                <div>
-                  <span>Nombre {e.name}</span>
+            <MarkerF position={e.posicion} onClick={(evt) => handleMouseOver(evt,e.posicion)}
+            > {info && (
+              <InfoWindow position={e.posicion}>
+                <div className="info-window">
+                  <div className="info-label"><span>Nombre de la oferta: {e.name}</span></div>
+                  <div className="info-label"><span>Descripción: {e.description}</span></div>
+                  <div className="info-label"><span>Requisitos: {e.requirement}</span></div>
+                  <div className="info-label"><span>Paga: {e.cash}</span></div>
+                  <button className="info-btn">Aceptar trabajo</button>
                 </div>
               </InfoWindow>
+            )}
+              
+              
             </MarkerF>
             )
           }
