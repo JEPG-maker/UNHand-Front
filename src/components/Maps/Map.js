ca28e59a5fcdf115b23222a99ff160b0e1a4c41d
Colmirage <semolinag@unal.edu.co>
2023-10-29 19:50:25 -0500

    version 5 mapa con y datos de trabajo

--- src/components/Maps/Map.js
+++ src/components/Maps/Map.js
@@ -14,56 +14,74 @@ export default function Ofertass() {
 
 function Map() {
   const datos = [
-    {posicion:{lat: 4.666,lng: -74.084}, name: '1', description: 'a', requirement:'qwe', cash: '123'},
-    {posicion:{lat: 4.675,lng: -74.100}, name: '2', description: 'b', requirement:'rty', cash: '312'},
-    {posicion:{lat: 4.666,lng: -74.107}, name: '3', description: 'c', requirement:'sdf', cash: '345'},
-    {posicion:{lat: 4.645,lng: -74.112}, name: '4', description: 'd', requirement:'xcv', cash: '654'}
+    {posicion:{lat: 4.666,lng: -74.084}, name: 'Oferta 1', description: 'mucho', requirement:'trabajo', cash: '123000'},
+    {posicion:{lat: 4.675,lng: -74.100}, name: 'oferta 2', description: 'poco', requirement:'flojo', cash: '312000'},
+    {posicion:{lat: 4.666,lng: -74.107}, name: 'oferta 3', description: 'vamos', requirement:'activo', cash: '345000'},
+    {posicion:{lat: 4.645,lng: -74.112}, name: 'oferta 4', description: 'cerca', requirement:'pasivo', cash: '654000'}
   ];
 
-  const [info, setInfo] = useState(false)
+  const [nombre, setNombre] = useState(false)
+  const [data, setData] = useState([])
+  const [mostrar, setMostrar] = useState(false)
 
   const center = useMemo(() => ({lat: 4.659372152302655,lng: -74.09146471557196}), []);
 
-  const handleMouseOver = (e, posicion) => {
-    //if (info === false) {
-      setInfo(posicion)
-      console.log(info)
-    } //else setInfo(false)
-      //console.log(info)
-    //setInfo(posicion)
-  
+  const handleMouseOver = (e, name) => {
+    setNombre(name)
+    console.log(name)
+  };
 
+  const handleMouseExit = e => {
+    setNombre(false)
+  };
+
+  const handleClick = (e, name, description, requirement, cash) => {
+    setData(['Oferta: '+name, 'Descripcion: '+description, 'Requisitos: '+requirement, 'Pago: '+cash])
+    setMostrar(true)
+    console.log(data)
+  };
+
+  
   
 
   return (
     <>
       <h2 className="bienvenida">Bienvenido</h2>
-      <GoogleMap zoom={15} center={center} mapContainerClassName="map-container">
+      <GoogleMap zoom={14} center={center} mapContainerClassName="map-container">
         <div>
           {
             datos.map((e) => 
-            <MarkerF position={e.posicion} onClick={(evt) => handleMouseOver(evt,e.posicion)}
-            > {info && (
-              <InfoWindow position={e.posicion}>
+            <MarkerF position={e.posicion} onMouseOver={(evt) => handleMouseOver(evt,e.description)}
+            onMouseOut={handleMouseExit}
+            onClick={(evt) => handleClick(evt, e.name, e.description, e.requirement, e.cash)}
+            > {nombre  && (
+              <InfoWindow>
                 <div className="info-window">
-                  <div className="info-label"><span>Nombre de la oferta: {e.name}</span></div>
-                  <div className="info-label"><span>Descripción: {e.description}</span></div>
-                  <div className="info-label"><span>Requisitos: {e.requirement}</span></div>
-                  <div className="info-label"><span>Paga: {e.cash}</span></div>
-                  <button className="info-btn">Aceptar trabajo</button>
+                  <div className="infow-label"><span>Oferta: {e.name}</span></div>
                 </div>
               </InfoWindow>
             )}
-              
-              
             </MarkerF>
             )
           }
         </div>
-        
       </GoogleMap>
-      
-
+      <div className="info-container">
+      {
+        data.map((d) =>
+        <div>
+        {mostrar && (
+          <div className="info-label">
+            <div>
+              <span>{d}</span>
+            </div>
+          </div>
+        )}
+        </div>
+        )
+      }
+      <button className="info-btn">Aceptar</button>
+      </div>
     </>
   );
 }
